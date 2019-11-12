33dc9fe8857c205160214d2baf9d6c8600a50d9a
Augustus Otu <Augani7@gmail.com>
2019-11-12 15:52:49 +0000

    changed port

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  port: process.env.PORT,
-  DBuser: process.env.USERNAME,
-  DBpassword: process.env.PASSWORD
+  port: process.env.PORT || 80,
+  
 };
