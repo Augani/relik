e5dc96c7132e3fd7b346324f6d65e0e4dcc32b1d
Augustus Otu <Augani7@gmail.com>
2019-11-12 15:59:27 +0000

    added build for production

---
+++ client/build/precache-manifest.37e2938fd965b1b80272c67db6518f15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e0ea81f634dca97de0ff657ac8dc8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7855935e3717ae8789b",
+    "url": "/static/css/2.6af22866.chunk.css"
+  },
+  {
+    "revision": "219c2797be3aa00bdc71",
+    "url": "/static/css/main.fd8dddb0.chunk.css"
+  },
+  {
+    "revision": "c7855935e3717ae8789b",
+    "url": "/static/js/2.8889f2b4.chunk.js"
+  },
+  {
+    "revision": "219c2797be3aa00bdc71",
+    "url": "/static/js/main.8722a32e.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "6a107bd3f1767c255fe54f6294464c26",
+    "url": "/static/media/mainHero.6a107bd3.svg"
+  },
+  {
+    "revision": "4bb94d004a251c784b9a3db79a77dafa",
+    "url": "/static/media/trend.4bb94d00.svg"
+  }
+]);
